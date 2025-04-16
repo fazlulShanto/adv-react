c203d01f16a615dccd8ac70ebdbc3b0125ebd936
Fazlul Hoque <fazlul@myalice.ai>
2025-04-17 01:03:43 +0600

    myth: prop changes triggers rerender

--- src/pages/counter/Counter.tsx
+++ src/pages/counter/Counter.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/components/Layout";
+
 import CounterCard1 from "./CounterCard1";
 import CounterCard2 from "./CounterCard2";
 import CounterCard3 from "./CounterCard3";
@@ -7,7 +7,7 @@ import PrintCard from "./PrintCard";
 
 export function Counter() {
     return (
-        <Layout className="p-8 flex flex-col gap-8">
+        <div className="p-8 flex flex-col gap-8">
             <h1 className="page-header"> Counter Page</h1>
             <div className="w-full border border-gray-700 rounded-md p-12 flex gap-4 flex-wrap">
                 <CounterCard1 />
@@ -16,7 +16,7 @@ export function Counter() {
                 <CounterCard4 />
                 <PrintCard />
             </div>
-        </Layout>
+        </div>
     );
 }
 
--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -1,14 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Counter from "../pages/counter/Counter";
 import Home from "@/pages/home/Home";
+import { PropDoesnotReRender } from "@/pages/PropDoesntReRender";
+import Layout from "@/components/Layout";
+
 
 export const router = createBrowserRouter([
     {
-        index: true,
-        element: <Home />,
-    },
-    {
-        path: "/counter",
-        element: <Counter />,
-    },
+        path : '/',
+        element : <Layout />,
+        children:[
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "/counter",
+                element: <Counter />,
+            },
+            {
+                "path":"/prop-rerender",
+                element : <PropDoesnotReRender />
+            }
+        ]
+    }
 ]);
