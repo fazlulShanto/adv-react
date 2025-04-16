70754d79f48f298bc29a47ef41e76c9b6263eed8
Fazlul Hoque <fazlul@myalice.ai>
2025-04-17 02:12:26 +0600

    moving-state-down-fix

--- src/pages/moving-state-down/MovingStateDown.tsx
+++ src/pages/moving-state-down/MovingStateDown.tsx
@@ -1,5 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  // DialogTrigger,
+} from "@/components/ui/dialog";
 
 const waitUtils = (ms: number) => {
   const start = Date.now();
@@ -26,12 +35,33 @@ export const AnotherVerySlowComponent = () => {
   );
 };
 
+const SubscriptionModal = () => {
+  const [localIsOpen, setLocalIsOpen] = useState(false);
+  return (
+    <Dialog open={localIsOpen} onOpenChange={setLocalIsOpen}>
+      <DialogTrigger className="border border-green-900 p-3 rounded bg-green-700 hover:bg-green-700">
+        Manage Subscription
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
 export const MovingStateDown = () => {
   const [randomValue, setRandomValue] = useState(Math.random());
 
   return (
     <div className="p-8 space-y-6">
       <h1 className="text-4xl text-blue-300 text-center">Moving State Down</h1>
+      <SubscriptionModal />
       <div className="flex flex-col justify-center items-center gap-4 border rounded-lg p-4">
         <p>Random Value : {randomValue}</p>
         <Button
